771c90c455c7ad11a769cbaa3a833230be7e9e47
Ranbir Sihota <ranbirsihota@gmail.com>
2018-02-04 17:22:11 -0500

    Conditionals

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -4,7 +4,8 @@ console.log("App.js is running");
 
 var app = {
     title: 'Indecision App',
-    subtitle: 'Put your life in the hands of a computer'
+    subtitle: 'Put your life in the hands of a computer',
+    options: ['One', 'Two']
 };
 
 // JSX - JavaScript XML
@@ -16,11 +17,48 @@ var template = React.createElement(
         null,
         app.title
     ),
-    React.createElement(
+    app.subtitle && React.createElement(
         'p',
         null,
         app.subtitle
+    ),
+    React.createElement(
+        'p',
+        null,
+        app.options.length > 0 ? 'Here are your options' : 'No options'
     )
+);
+
+var user = {
+    name: 'Ranbir Sihota',
+    age: 25,
+    location: 'Mississauga'
+};
+function getLocation(location) {
+    if (location) {
+        return React.createElement(
+            'p',
+            null,
+            'Location: ',
+            location
+        );
+    }
+}
+var templateTwo = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        user.name ? user.name : 'Anonymous'
+    ),
+    user.age && user.age >= 18 && React.createElement(
+        'p',
+        null,
+        'Age: ',
+        user.age
+    ),
+    getLocation(user.location)
 );
 
 var appRoot = document.getElementById('app');
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -2,14 +2,34 @@ console.log("App.js is running");
 
 var app = {
     title: 'Indecision App',
-    subtitle: 'Put your life in the hands of a computer'
+    subtitle: 'Put your life in the hands of a computer',
+    options: ['One', 'Two']
 };
 
 // JSX - JavaScript XML
 var template = (
     <div>
         <h1>{app.title}</h1>
-        <p>{app.subtitle}</p>
+        {app.subtitle && <p>{app.subtitle}</p>}
+        <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
+    </div>
+);
+
+var user = {
+    name: 'Ranbir Sihota',
+    age: 25,
+    location: 'Mississauga'
+};
+function getLocation(location) {
+    if (location) {
+        return <p>Location: {location}</p>;
+    }
+}
+var templateTwo = (
+    <div>
+        <h1>{user.name ? user.name : 'Anonymous'}</h1>
+        {(user.age && user.age >=18) && <p>Age: {user.age}</p>}
+        {getLocation(user.location)}
     </div>
 );
 
