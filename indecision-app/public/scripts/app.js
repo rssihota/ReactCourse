17c8a0fac6ebeba9b29c7362e017b3733f813196
Ranbir Sihota <ranbirsihota@gmail.com>
2018-02-04 16:53:16 -0500

    JSX Expression

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,13 +1,28 @@
-"use strict";
+'use strict';
 
 console.log("App.js is running");
 
+var app = {
+    title: 'Indecision App',
+    subtitle: 'Put your life in the hands of a computer'
+};
+
 // JSX - JavaScript XML
 var template = React.createElement(
-  "h1",
-  null,
-  "Indecision App"
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        app.title
+    ),
+    React.createElement(
+        'p',
+        null,
+        app.subtitle
+    )
 );
+
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,7 +1,18 @@
 console.log("App.js is running");
 
+var app = {
+    title: 'Indecision App',
+    subtitle: 'Put your life in the hands of a computer'
+};
+
 // JSX - JavaScript XML
-var template = <h1>Indecision App</h1>;
+var template = (
+    <div>
+        <h1>{app.title}</h1>
+        <p>{app.subtitle}</p>
+    </div>
+);
+
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
