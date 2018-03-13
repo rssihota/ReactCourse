7a6de01603d4f43f5c9c617b4aec3893563c6354
Ranbir Sihota <ranbirsihota@gmail.com>
2018-03-12 20:45:38 -0400

    ES6 Classes 1

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,40 +1,37 @@
 'use strict';
 
-console.log("App.js is running");
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-var app = {
-    title: 'Visibility Toggle',
-    text: 'Hey, this is the secret text',
-    isVisible: false
-};
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-var toggle = function toggle() {
-    app.isVisible = !app.isVisible;
-    render();
-};
+var Person = function () {
+    function Person() {
+        var name = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'Anonymous';
+        var age = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 0;
 
-var render = function render() {
-    var template = React.createElement(
-        'div',
-        null,
-        React.createElement(
-            'h1',
-            null,
-            app.title
-        ),
-        React.createElement(
-            'button',
-            { onClick: toggle },
-            app.isVisible ? 'Hide Details' : 'Show Details'
-        ),
-        React.createElement(
-            'p',
-            null,
-            app.isVisible ? app.text : ''
-        )
-    );
+        _classCallCheck(this, Person);
 
-    ReactDOM.render(template, document.getElementById('app'));
-};
+        this.name = name;
+        this.age = age;
+    }
 
-render();
+    _createClass(Person, [{
+        key: 'getGreeting',
+        value: function getGreeting() {
+            return 'Hi. I am ' + this.name + '!';
+        }
+    }, {
+        key: 'getDescription',
+        value: function getDescription() {
+            return this.name + ' is ' + this.age + ' year(s) old.';
+        }
+    }]);
+
+    return Person;
+}();
+
+var me = new Person('Ranbir Sihota', 25);
+console.log(me.getDescription());
+
+var other = new Person();
+console.log(other.getDescription());
