691bdb2b7a3bccf26884340e9b05364bcd1382fd
Ranbir Sihota <ranbirsihota@gmail.com>
2018-03-12 21:06:29 -0400

    ES6 Classes 2

--- indecision-app/src/playground/es6-classes-1.js
+++ indecision-app/src/playground/es6-classes-1.js
@@ -12,8 +12,39 @@ class Person {
     }
 }
 
-const me = new Person('Ranbir Sihota', 25);
-console.log(me.getDescription());
+class Student extends Person {
+    constructor(name, age, major) {
+        super(name, age);
+        this.major = major;
+    }
+    hasMajor() {
+        return !!this.major;
+    }
+    getDescription() {
+        let description = super.getDescription();
+        if (this.hasMajor()) {
+            description += ` Their major is ${this.major}.`;
+        }
+        return description;
+    }
+}
 
-const other = new Person();
-console.log(other.getDescription());
+class Traveler extends Person {
+    constructor(name, age, homeLocation) {
+        super(name, age);
+        this.homeLocation = homeLocation;
+    }
+    getGreeting() {
+        let greeting = super.getGreeting();
+        if (this.homeLocation) {
+            greeting += ` I'm visiting from ${this.homeLocation}.`;
+        }
+        return greeting;
+    }
+}
+
+const me = new Traveler('Ranbir Sihota', 25, 'Toronto');
+console.log(me.getGreeting());
+
+const other = new Traveler();
+console.log(other.getGreeting());
